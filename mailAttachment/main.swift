df08db0e55aca216faa9f599834c8429c16720f3
Dave Reed <dave256@mac.com>
2020-09-06 11:42:42 -0400

    specify subject a minor refactoring

--- mailAttachment/main.swift
+++ mailAttachment/main.swift
@@ -17,14 +17,14 @@ import AppleScriptObjC
 ///   - recipient: email address of the recipient
 ///   - fileURL: URL of file to send
 /// - Returns: String containing AppleScript command that when executed will send the email
-func createAppleScript(emailSender: String, emailRecipient: String, fileURL: URL) -> String {
+func createAppleScript(emailSender: String, subject: String, emailRecipient: String, fileURL: URL) -> String {
 
     let script = """
     set p to "\(fileURL.path)"
     set theAttachment to POSIX file p
 
     tell application "Mail"
-        set theNewMessage to make new outgoing message with properties {subject:"attached file", sender:"\(emailSender)", content:"see attached\n", visible:true}
+        set theNewMessage to make new outgoing message with properties {subject:"\(subject)", sender:"\(emailSender)", content:"See attached\n\n", visible:true}
 
         tell theNewMessage
             make new to recipient at end of to recipients with properties {address:"\(emailRecipient)"}
@@ -49,11 +49,28 @@ func createAppleScript(emailSender: String, emailRecipient: String, fileURL: URL
     return script
 }
 
+fileprivate func executeScript(_ script: String) -> String {
+    var msg = ""
+
+    // execute the AppleScript
+    var error: NSDictionary?
+    if let scriptObject = NSAppleScript(source: script) {
+        let output = scriptObject.executeAndReturnError(&error)
+        if error != nil {
+            msg = "error: \(error!)"
+        }
+        else if let s = output.stringValue {
+            msg = s
+        }
+    }
+    return msg
+}
+
 /// for each subdirectory in directory, attach one file and email to the email address specified by subdirectory name
 /// - Parameters:
 ///   - emailSender: email account to use to send (must be a valid account in Mail app)
 ///   - directory: URL of directory containing all the subdirectories that are email address names
-func sendEmails(emailSender: String, directory: String) {
+func sendEmails(emailSender: String, subject: String, directory: String) {
     let fm = FileManager()
     let url = URL(fileURLWithPath: directory)
     if let contents = try? fm.contentsOfDirectory(at: url, includingPropertiesForKeys: [.isDirectoryKey], options: [.skipsSubdirectoryDescendants, .skipsHiddenFiles]) {
@@ -63,18 +80,12 @@ func sendEmails(emailSender: String, directory: String) {
                     let directoryContents = try? fm.contentsOfDirectory(at: content, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles, .skipsSubdirectoryDescendants, .skipsPackageDescendants])
                     if let firstURL = directoryContents?.first {
                         let emailRecipient = content.lastPathComponent
-                        let script = createAppleScript(emailSender: emailSender, emailRecipient: emailRecipient, fileURL: firstURL)
+                        let script = createAppleScript(emailSender: emailSender, subject: subject, emailRecipient: emailRecipient, fileURL: firstURL)
 
-                        // execute the AppleScript
-                        var error: NSDictionary?
-                        if let scriptObject = NSAppleScript(source: script) {
-                            let output = scriptObject.executeAndReturnError(&error)
-                            if error != nil {
-                                print("error: \(error!)")
-                            }
-                            else if let msg = output.stringValue {
-                                print(msg)
-                            }
+                        let msg = executeScript(script)
+                        if msg != "" {
+                            print(emailRecipient, terminator: " ")
+                            print(msg)
                         }
                     }
                 }
@@ -84,12 +95,14 @@ func sendEmails(emailSender: String, directory: String) {
 }
 
 struct EmailSender: ParsableCommand {
+    @Option(name: .shortAndLong, help: "subject line for email.")
+       var subject: String = "attached file"
     @Argument() var sender: String
 
     func run() {
         let fm = FileManager()
         let cwd = fm.currentDirectoryPath
-        sendEmails(emailSender: sender, directory: cwd)
+        sendEmails(emailSender: sender, subject: subject, directory: cwd)
     }
 }
 
